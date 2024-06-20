a6c62d0cea6cfeefeab92e05ae81d603b511e5eb
Mahlet Wondesen <mahletwondesen448@gmail.com>
2024-06-20 23:36:07 +0300

    Update Song.js

--- backend/src/models/Song.js
+++ backend/src/models/Song.js
@@ -1,6 +1,6 @@
 import { Schema, model } from 'mongoose';
 
-// Define the Mongoose schema for the Song model
+
 const songSchema = new Schema({
   title: { type: String, required: true },
   artist: { type: String, required: true },
@@ -8,7 +8,7 @@ const songSchema = new Schema({
   genre: { type: String, required: true },
 });
 
-// Create and export the Mongoose model based on the schema
+
 const Song = model('Song', songSchema);
 
 export default Song;
